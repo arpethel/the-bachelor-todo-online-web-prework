4ca6b0bf2f561dbbf29a321cd7bc6bcc1aff4efe
Ryan Pethel <arpethel@gmail.com>
2019-03-18 08:34:47 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,33 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  # increment data's keys
+  # increment data's values
+  data.each do |k, v|
+    k_i = 0
+    v_i = 0
+    while k_i < k.length
+      while v_i < v.length
+        v[v_i].each do |key, value|
+          # binding.pry
+          if value == "Winner"
+            winner_full_name = v[v_i]["name"]
+            winner_first_name = winner_full_name.split
+            winner = winner_first_name[0]
+            # return winner
+            # if winner_first_name.length > 1
+            #   winner =  winner_first_name[0]
+            # else
+            #   winner = winner_full_name
+            # end
+          end
+          # binding.pry
+        end
+        v_i += 1
+      end
+      k_i += 1
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
